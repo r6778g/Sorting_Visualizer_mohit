28f265317645a1b248e497613eba3fd54df54e27
mohit-divtechnosoft <142472398+r6778g@users.noreply.github.com>
2025-08-11 15:48:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,88 +1,97 @@
-
 /*
-Variable naming convention: <object>_<action>_<objectname>; Example -> Button_click_b1;
+Variable naming convention: <object>_<action>_<objectname>
+Example -> button_click_b1
 */
 
-//Variables (BE CAREFUL THESE MIGHT BE USED IN OTHER JS FILES TOO)
-var inp_as=document.getElementById('a_size'),array_size=inp_as.value;
-var inp_gen=document.getElementById("a_generate");
-var inp_aspeed=document.getElementById("a_speed");
-//var array_speed=document.getElementById('a_speed').value;
+// ===== DOM Elements =====
+const input_arraySize = document.getElementById("a_size");
+let array_size = Number(input_arraySize.value);
 
-var butts_algos=document.querySelectorAll(".algos button");
+const button_generateArray = document.getElementById("a_generate");
+const input_arraySpeed = document.getElementById("a_speed");
+const buttons_algorithms = document.querySelectorAll(".algos button");
 
-var div_sizes=[];
-var divs=[];
-var margin_size;
-var cont=document.getElementById("array_container");
-cont.style="flex-direction:row";
+const container_array = document.getElementById("array_container");
+container_array.style.flexDirection = "row";
 
-//Array generation and updation.
+// ===== Variables =====
+let div_sizes = [];
+let divs = [];
+let margin_size = 0.1;
 
-inp_gen.addEventListener("click",generate_array);
-inp_as.addEventListener("input",update_array_size);
+// ===== Event Listeners =====
+button_generateArray.addEventListener("click", generateArray);
+input_arraySize.addEventListener("input", updateArraySize);
 
-function generate_array()
-{
-    cont.innerHTML="";
+window.onload = updateArraySize;
 
-    for(var i=0;i<array_size;i++)
-    {
-        div_sizes[i]=Math.floor(Math.random() * 0.5*(inp_as.max - inp_as.min) ) + 10;
-        divs[i]=document.createElement("div");
-        cont.appendChild(divs[i]);
-        margin_size=0.1;
-        divs[i].style=" margin:0% " + margin_size + "%; background-color:blue; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
+buttons_algorithms.forEach(button => {
+    button.addEventListener("click", runAlgorithm);
+});
+
+// ===== Functions =====
+
+// Generate the array and its visual representation
+function generateArray() {
+    container_array.innerHTML = "";
+
+    for (let i = 0; i < array_size; i++) {
+        const height = Math.floor(Math.random() * 0.5 * (input_arraySize.max - input_arraySize.min)) + 10;
+        div_sizes[i] = height;
+
+        const bar = document.createElement("div");
+        bar.style.margin = `0% ${margin_size}%`;
+        bar.style.backgroundColor = "blue";
+        bar.style.width = `${100 / array_size - 2 * margin_size}%`;
+        bar.style.height = `${height}%`;
+
+        divs[i] = bar;
+        container_array.appendChild(bar);
     }
 }
 
-function update_array_size()
-{
-    array_size=inp_as.value;
-    generate_array();
+// Update array size and regenerate
+function updateArraySize() {
+    array_size = Number(input_arraySize.value);
+    generateArray();
 }
 
-window.onload=update_array_size();
+// Disable UI interactions during sorting
+function disableButtons() {
+    buttons_algorithms.forEach(button => {
+        button.classList.remove(...button.classList);
+        button.classList.add("butt_locked");
+        button.disabled = true;
+    });
 
-//Running the appropriate algorithm.
-for(var i=0;i<butts_algos.length;i++)
-{
-    butts_algos[i].addEventListener("click",runalgo);
+    input_arraySize.disabled = true;
+    button_generateArray.disabled = true;
+    input_arraySpeed.disabled = true;
 }
 
-function disable_buttons()
-{
-    for(var i=0;i<butts_algos.length;i++)
-    {
-        butts_algos[i].classList=[];
-        butts_algos[i].classList.add("butt_locked");
-
-        butts_algos[i].disabled=true;
-        inp_as.disabled=true;
-        inp_gen.disabled=true;
-        inp_aspeed.disabled=true;
-    }
-}
-
-function runalgo()
-{
-    disable_buttons();
-
+// Run selected sorting algorithm
+function runAlgorithm() {
+    disableButtons();
     this.classList.add("butt_selected");
-    switch(this.innerHTML)
-    {
-        case "Bubble":Bubble();
-                        break;
-        case "Selection":Selection_sort();
-                        break;
-        case "Insertion":Insertion();
-                        break;
-        case "Merge":Merge();
-                        break;
-        case "Quick":Quick();
-                        break;
-        case "Heap":Heap();
-                        break;
+
+    switch (this.innerText.trim()) {
+        case "Bubble":
+            Bubble();
+            break;
+        case "Selection":
+            Selection_sort();
+            break;
+        case "Insertion":
+            Insertion();
+            break;
+        case "Merge":
+            Merge();
+            break;
+        case "Quick":
+            Quick();
+            break;
+        case "Heap":
+            Heap();
+            break;
     }
 }
-
