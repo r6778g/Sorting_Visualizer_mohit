b2147cd7f057100a82101388450d1879bee884a6
mohit-divtechnosoft <142472398+r6778g@users.noreply.github.com>
2025-08-12 11:22:09 +0530

    Update insertion_sort.js

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,64 +1,53 @@
 /*
     *****************
     DONE BY:-   TUMMALA KETHAN
-    
+    Refactored for readability and maintainability
     *****************
 */
 
-function Insertion()
-{
-    //Setting Time complexities
-    document.getElementById("Time_Worst").innerText="O(N^2)";
-    document.getElementById("Time_Average").innerText="Θ(N^2)";
-    document.getElementById("Time_Best").innerText="Ω(N)";
+function Insertion() {
+    // Set Time complexities
+    document.getElementById("Time_Worst").innerText = "O(N²)";
+    document.getElementById("Time_Average").innerText = "Θ(N²)";
+    document.getElementById("Time_Best").innerText = "Ω(N)";
 
-    //Setting Space complexity
-    document.getElementById("Space_Worst").innerText="O(1)";
+    // Set Space complexity
+    document.getElementById("Space_Worst").innerText = "O(1)";
 
-    c_delay=0;
+    c_delay = 0;
 
-    for(var j=0;j<array_size;j++)
-    {
-        div_update(divs[j],div_sizes[j],"yellow");//Color update
+    for (let j = 0; j < array_size; j++) {
+        div_update(divs[j], div_sizes[j], "yellow"); // Current element being compared
 
-        var key= div_sizes[j];
-        var i=j-1;
-        while(i>=0 && div_sizes[i]>key)
-        {
-            div_update(divs[i],div_sizes[i],"red");//Color update
-            div_update(divs[i+1],div_sizes[i+1],"red");//Color update
+        let key = div_sizes[j];
+        let i = j - 1;
 
-            div_sizes[i+1]=div_sizes[i];
+        // Shift elements greater than key to the right
+        while (i >= 0 && div_sizes[i] > key) {
+            div_update(divs[i], div_sizes[i], "red");     // Comparing element
+            div_update(divs[i + 1], div_sizes[i + 1], "red");
 
-            div_update(divs[i],div_sizes[i],"red");//Height update
-            div_update(divs[i+1],div_sizes[i+1],"red");//Height update
-    
-            div_update(divs[i],div_sizes[i],"blue");//Color update
-            if(i==(j-1))
-            {
-                div_update(divs[i+1],div_sizes[i+1],"yellow");//Color update
-            }
-            else
-            {
-                div_update(divs[i+1],div_sizes[i+1],"blue");//Color update
-            }
-            i-=1;
+            div_sizes[i + 1] = div_sizes[i];             // Shift value right
+
+            div_update(divs[i], div_sizes[i], "red");     // Height update
+            div_update(divs[i + 1], div_sizes[i + 1], "red");
+
+            div_update(divs[i], div_sizes[i], "blue");    // Revert color
+            div_update(divs[i + 1], div_sizes[i + 1], 
+                i === (j - 1) ? "yellow" : "blue"
+            );
+
+            i--;
         }
-        div_sizes[i+1]=key;
 
-        for(var t=0;t<j;t++)
-        {
-            div_update(divs[t],div_sizes[t],"green");//Color update
+        // Place the key at correct position
+        div_sizes[i + 1] = key;
+
+        // Mark sorted portion in green
+        for (let t = 0; t <= j; t++) {
+            div_update(divs[t], div_sizes[t], "green");
         }
     }
-    div_update(divs[j-1],div_sizes[j-1],"green");//Color update
 
-    enable_buttons();
+    enable_buttons(); // Re-enable UI controls
 }
-
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
