fab5718cf4fc403494adfa8461920be482b61a15
mohit-divtechnosoft <142472398+r6778g@users.noreply.github.com>
2025-08-20 11:25:07 +0530

    Update bubble_sort.js

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,41 +1,45 @@
+function Bubble() {
+    // Setting Time complexities
+    document.getElementById("Time_Worst").innerText = "O(N^2)";
+    document.getElementById("Time_Average").innerText = "Θ(N^2)";
+    document.getElementById("Time_Best").innerText = "Ω(N)";
 
+    // Setting Space complexity
+    document.getElementById("Space_Worst").innerText = "O(1)";
 
-function Bubble()
-{
-    //Setting Time complexities
-    document.getElementById("Time_Worst").innerText="O(N^2)";
-    document.getElementById("Time_Average").innerText="Θ(N^2)";
-    document.getElementById("Time_Best").innerText="Ω(N)";
+    c_delay = 0;
 
-    //Setting Space complexity
-    document.getElementById("Space_Worst").innerText="O(1)";
+    for (let i = 0; i < array_size - 1; i++) {
+        for (let j = 0; j < array_size - i - 1; j++) {
+            // Highlight current comparison
+            div_update(divs[j], div_sizes[j], "yellow");
+            div_update(divs[j + 1], div_sizes[j + 1], "yellow");
 
-    c_delay=0;
+            if (div_sizes[j] > div_sizes[j + 1]) {
+                // Swap with highlight
+                div_update(divs[j], div_sizes[j], "red");
+                div_update(divs[j + 1], div_sizes[j + 1], "red");
 
-    for(var i=0;i<array_size-1;i++)
-    {
-        for(var j=0;j<array_size-i-1;j++)
-        {
-            div_update(divs[j],div_sizes[j],"yellow");//Color update
+                let temp = div_sizes[j];
+                div_sizes[j] = div_sizes[j + 1];
+                div_sizes[j + 1] = temp;
 
-            if(div_sizes[j]>div_sizes[j+1])
-            {
-                div_update(divs[j],div_sizes[j], "red");//Color update
-                div_update(divs[j+1],div_sizes[j+1], "red");//Color update
-
-                var temp=div_sizes[j];
-                div_sizes[j]=div_sizes[j+1];
-                div_sizes[j+1]=temp;
-
-                div_update(divs[j],div_sizes[j], "red");//Height update
-                div_update(divs[j+1],div_sizes[j+1], "red");//Height update
+                // Update swapped heights
+                div_update(divs[j], div_sizes[j], "red");
+                div_update(divs[j + 1], div_sizes[j + 1], "red");
             }
-            div_update(divs[j],div_sizes[j], "blue");//Color updat
+
+            // Restore colors after comparison
+            div_update(divs[j], div_sizes[j], "blue");
+            div_update(divs[j + 1], div_sizes[j + 1], "blue");
         }
-        div_update(divs[j],div_sizes[j], "green");//Color update
+
+        // Mark the last sorted element
+        div_update(divs[array_size - i - 1], div_sizes[array_size - i - 1], "green");
     }
-    div_update(divs[0],div_sizes[0], "green");//Color update
+
+    // Finally, mark the first element as sorted
+    div_update(divs[0], div_sizes[0], "green");
 
     enable_buttons();
 }
-
