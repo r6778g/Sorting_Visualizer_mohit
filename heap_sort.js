e78a4b58fbcccd9468b11cec2ff408357ecbc3a2
mohit-divtechnosoft <142472398+r6778g@users.noreply.github.com>
2025-08-20 11:04:42 +0530

    Update heap_sort.js

--- heap_sort.js
+++ heap_sort.js
@@ -1,95 +1,79 @@
+function Heap() {
+    // Setting Time complexities
+    document.getElementById("Time_Worst").innerText = "O(N log N)";
+    document.getElementById("Time_Average").innerText = "Θ(N log N)";
+    document.getElementById("Time_Best").innerText = "Ω(N log N)";
 
+    // Setting Space complexity
+    document.getElementById("Space_Worst").innerText = "O(1)";
 
-function Heap()
-{
-    //Setting Time complexities
-    document.getElementById("Time_Worst").innerText="O(N log N)";
-    document.getElementById("Time_Average").innerText="Θ(N log N)";
-    document.getElementById("Time_Best").innerText="Ω(N log N)";
-
-    //Setting Space complexity
-    document.getElementById("Space_Worst").innerText="O(1)";
-
-    c_delay=0;
+    c_delay = 0;
 
     heap_sort();
-    
+
     enable_buttons();
 }
 
-function swap(i,j)
-{
-    div_update(divs[i],div_sizes[i],"red");//Color update
-    div_update(divs[j],div_sizes[j],"red");//Color update
+function swap(i, j) {
+    div_update(divs[i], div_sizes[i], "red");   // highlight
+    div_update(divs[j], div_sizes[j], "red");
 
-    var temp=div_sizes[i];
-    div_sizes[i]=div_sizes[j];
-    div_sizes[j]=temp;
+    let temp = div_sizes[i];
+    div_sizes[i] = div_sizes[j];
+    div_sizes[j] = temp;
 
-    div_update(divs[i],div_sizes[i],"red");//Height update
-    div_update(divs[j],div_sizes[j],"red");//Height update
+    div_update(divs[i], div_sizes[i], "red");   // update height
+    div_update(divs[j], div_sizes[j], "red");
 
-    div_update(divs[i],div_sizes[i],"blue");//Color update
-    div_update(divs[j],div_sizes[j],"blue");//Color update
+    div_update(divs[i], div_sizes[i], "blue");  // restore
+    div_update(divs[j], div_sizes[j], "blue");
 }
 
-function max_heapify(n,i)
-{
-    var largest=i;
-    var l=2*i+1;
-    var r=2*i+2;
+function max_heapify(n, i) {
+    let largest = i;
+    let l = 2 * i + 1;
+    let r = 2 * i + 2;
 
-    if(l<n && div_sizes[l]>div_sizes[largest])
-    {
-        if(largest!=i)
-        {
-            div_update(divs[largest],div_sizes[largest],"blue");//Color update
-        }
-
-        largest=l;
-
-        div_update(divs[largest],div_sizes[largest],"red");//Color update
+    // Check left child
+    if (l < n && div_sizes[l] > div_sizes[largest]) {
+        largest = l;
     }
 
-    if(r<n && div_sizes[r]>div_sizes[largest])
-    {
-        if(largest!=i)
-        {
-            div_update(divs[largest],div_sizes[largest],"blue");//Color update
-        }
-
-        largest=r;
-
-        div_update(divs[largest],div_sizes[largest],"red");//Color update
+    // Check right child
+    if (r < n && div_sizes[r] > div_sizes[largest]) {
+        largest = r;
     }
 
-    if(largest!=i)
-    {
-        swap(i,largest);
+    // If root is not largest, swap and continue heapify
+    if (largest !== i) {
+        div_update(divs[i], div_sizes[i], "red");       // highlight parent
+        div_update(divs[largest], div_sizes[largest], "red");
 
-        max_heapify(n,largest);
+        swap(i, largest);
+
+        div_update(divs[i], div_sizes[i], "blue");      // restore
+        div_update(divs[largest], div_sizes[largest], "blue");
+
+        max_heapify(n, largest);
     }
 }
 
-function heap_sort()
-{
-    for(var i=Math.floor(array_size/2)-1;i>=0;i--)
-    {
-        max_heapify(array_size,i);
+function heap_sort() {
+    // Step 1: Build max heap
+    for (let i = Math.floor(array_size / 2) - 1; i >= 0; i--) {
+        max_heapify(array_size, i);
     }
 
-    for(var i=array_size-1;i>0;i--)
-    {
-        swap(0,i);
-        div_update(divs[i],div_sizes[i],"green");//Color update
-        div_update(divs[i],div_sizes[i],"yellow");//Color update
+    // Step 2: Extract elements one by one
+    for (let i = array_size - 1; i > 0; i--) {
+        div_update(divs[0], div_sizes[0], "red");  // highlight root
+        swap(0, i);
 
-        max_heapify(i,0);
+        div_update(divs[i], div_sizes[i], "green"); // mark sorted
 
-        div_update(divs[i],div_sizes[i],"blue");//Color update
-        div_update(divs[i],div_sizes[i],"green");//Color update
+        max_heapify(i, 0);
     }
-    div_update(divs[i],div_sizes[i],"green");//Color update
+
+    // Mark final element sorted
+    div_update(divs[0], div_sizes[0], "green");
 }
-
-
